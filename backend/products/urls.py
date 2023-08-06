2b82abdc0638e3658a62675080db37626d9686f8
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-06 02:30:44 +0000

    Initial commit

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("<int:pk>/", views.product_alt_view),
-    path("", views.product_alt_view)
+    path("", views.product_list_create_view)
 ]
