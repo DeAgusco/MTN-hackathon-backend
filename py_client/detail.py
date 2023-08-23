1cd2acfc70bf1533c58cb0ea548664b9437e87f4
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-23 18:26:48 +0000

    Initial Commit

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,6 +1,8 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/1"
-
-response = requests.get(endpoint)
-print(response.json())
+endpoint = "http://localhost:8000/api/wallet/"
+headers = {
+        "Authorization":"Bearer ad55517b6a0611c5a931e28a5532a173fd5795da"
+}
+response = requests.get(endpoint, headers=headers)
+print(response.json()['balance'])
--- py_client/real_time.py
+++ py_client/real_time.py
@@ -3,7 +3,7 @@ import json
 import websockets
 
 async def test():
-    async with websockets.connect("ws://localhost:8000/ws/transactions/?user_id=2") as websocket:
+    async with websockets.connect("ws://localhost:8000/ws/transactions/?user_id=1") as websocket:
         # Listen for messages
         while True:
             message = await websocket.recv()
