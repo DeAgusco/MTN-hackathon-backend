8591bcbe7d5972dd2c443812b0283bc716572252
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-31 01:16:45 +0000

    Update create.py

--- py_client/create.py
+++ py_client/create.py
@@ -1,10 +1,8 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/"
-
-data = {
-    "title":"Nunu",
-    "price":24.50
+endpoint = "http://localhost:8000/product/1/unlike/"
+headers = {
+        "Authorization":"Token 7df01d8099b78b1f816f6dee7eed2a634a9f76d7"
 }
-response = requests.post(endpoint, json=data)
+response = requests.post(endpoint, headers=headers)
 print(response.json())
